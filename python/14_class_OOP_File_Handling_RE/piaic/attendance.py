94f4c179ea70f7280cf4cea9b2eb90709a3cd33e
Sir Qasim <m.qasim077@gmail.com>
2022-10-16 12:04:18 +0500

    OOP_FileHandling

---
+++ python/14_class_OOP_File_Handling_RE/piaic/attendance.py
@@ -0,0 +1,11 @@
+def f1():
+    return "function1 body from attendance module"
+
+def f2():
+    return "function2 body from attendance module"
+
+def f3():
+    return "function3 body from attendance module"
+
+def f4():
+    return "function4 body from attendance module"
---
+++ python/14_class_OOP_File_Handling_RE/piaic/maths.py
@@ -0,0 +1,11 @@
+def f1():
+    return "function1 body from maths module"
+
+def f2():
+    return "function2 body from maths module"
+
+def f3():
+    return "function3 body from maths module"
+
+def f4():
+    return "function4 body from maths module"
